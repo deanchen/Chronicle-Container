d0e84db7a46ac218827805577450461484d17f78
Dean Chen <dean@ocirs.com>
2011-03-22 23:20:34 -0400

    Restructure for portability.

--- server.js
+++ server.js
@@ -4,23 +4,24 @@
 
 var express = require('express');
 var stylus = require('stylus');
-// Path to our public directory
 
-var pub = __dirname + '/public';
 // Auto-compile sass to css with "compiler"
 // and then serve with connect's staticProvider
-
 var app = express.createServer();
 
 // add the stylus middleware, which re-compiles when
 // a stylesheet has changed, compiling FROM src,
 // TO dest. dest is optional, defaulting to src
-
 app.use(stylus.middleware({
     src: __dirname + '/views'
   , dest: __dirname + '/public'
   , compile: compile
 }));
+
+// Path to our public directory
+var pub = __dirname + '/public';
+// Optional since express defaults to CWD/views
+app.set('views', __dirname + '/views');
 
 app.use(app.router);
 app.use(express.static(pub));
@@ -33,13 +34,8 @@ function compile(str, path, fn) {
     .render(fn);
 };
 
-// Optional since express defaults to CWD/views
-
-app.set('views', __dirname + '/views');
-
-// Set our default template engine to "jade"
-// which prevents the need for extensions
-// (although you can still mix and match)
+// Set our default template engine to "jade" which prevents the need for 
+// extensions (although you can still mix and match)
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
